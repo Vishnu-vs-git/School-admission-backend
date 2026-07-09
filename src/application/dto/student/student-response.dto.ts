6fed369a7ac0977dcdc8f278f273faf2b9a80d33
vishnu <getvishnuvs@gmail.com>
2026-07-09 21:55:22 +0530

    feat:implemented create student usecase

--- src/application/dto/student/student-response.dto.ts
+++ src/application/dto/student/student-response.dto.ts
@@ -1,10 +1,22 @@
+import { ApplicationStatus } from 'src/domain/enums/application-status.enum';
 import { Gender } from 'src/domain/enums/gender.enum';
 import { Grade } from 'src/domain/enums/grade.enum';
 
-export interface CreateStudentDto {
+export interface StudentResponseDto {
+  id: string;
   studentName: string;
   dateOfBirth: Date;
   gender: Gender;
   previousSchool: string;
   applyingGrade: Grade;
+
+  status: ApplicationStatus;
+
+  examScore?: number;
+
+  assignedCourse?: Grade;
+
+  createdAt?: Date;
+
+  updatedAt?: Date;
 }
--- src/application/mappers/student.mapper.ts
+++ src/application/mappers/student.mapper.ts
@@ -1,7 +1,8 @@
 import { Student } from 'src/domain/entities/student.entity';
-import { CreateStudentDto } from '../dto/student/student-response.dto';
-import { StudentResponseDto } from '../dto/student/create-student.dto';
+
 import { ApplicationStatus } from 'src/domain/enums/application-status.enum';
+import { StudentResponseDto } from '../dto/student/student-response.dto';
+import { CreateStudentDto } from '../dto/student/create-student.dto';
 
 export class StudentMapper {
   static toDomain(dto: CreateStudentDto, parentId: string): Student {
