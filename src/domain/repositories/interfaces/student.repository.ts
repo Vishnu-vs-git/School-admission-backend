3faee29770703e751947daa6dd251a0bcdd70d59
vishnu <getvishnuvs@gmail.com>
2026-07-09 21:35:44 +0530

    feat: implemented student repository

--- src/domain/repositories/interfaces/student.repository.ts
+++ src/domain/repositories/interfaces/student.repository.ts
@@ -3,11 +3,11 @@ import { Student } from 'src/domain/entities/student.entity';
 export interface IStudentRepository {
   create(student: Student): Promise<Student>;
 
-  update(student: Student): Promise<Student>;
+  update(student: Student): Promise<Student | null>;
 
   findById(id: string): Promise<Student | null>;
 
-  findByParent(parentId: string): Promise<Student[]>;
+  // findByParent(parentId: string): Promise<Student[]>;
 
   findAll(): Promise<Student[]>;
 }
